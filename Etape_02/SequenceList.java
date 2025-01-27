da5bafb86d241dba03c18da321c3ce242702a831
Nguyen Chu Hoang Anh <nguyenc@im2ag-mandelbrot2.u-ga.fr>
2025-01-27 21:24:51 +0100

    Add Etape 2

---
+++ Etape_02/SequenceList.java
@@ -0,0 +1,50 @@
+class SequenceList {
+    ListeChainee tete, queue;
+
+    void insereTete(int element) {
+        ListeChainee newElement = new ListeChainee(element, tete);
+        if (this.queue == null) {
+            this.queue = newElement;
+        }
+    }
+
+    void insereQueue(int element) {
+        ListeChainee newElement = new ListeChainee(element, null);
+        if (this.tete == null) {
+            this.tete = newElement;
+        } else {
+            this.queue.suiv = newElement;
+            this.queue = newElement;
+        }
+    }
+
+    int extraitTete() {
+        int result;
+        result = this.tete.val;
+        this.tete = this.tete.suiv;
+        if (this.tete == null) {
+            this.queue = null;
+        }
+        return result;
+    }
+
+    boolean estVide() {
+        return this.tete == null;
+    }
+
+    public String toString() {
+        String resultat = "SequenceListe [ ";
+        boolean premier = true;
+        ListeChainee m = tete;
+        while (m != null) {
+            if (!premier)
+                resultat += ", ";
+            resultat += m.val;
+            m = m.suiv;
+            premier = false;
+        }
+        resultat += " ]";
+        return resultat;
+    }
+
+}
